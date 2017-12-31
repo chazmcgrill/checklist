0b411afd71b38239fce68ed3bce43dbf549e9c32
chazmcgrill <charlietaylordev@gmail.com>
2017-12-31 18:47:56 +0000

    Initial Setup
    Created app component and setup react sass and webpack

---
+++ src/components/Checklist.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Checklist extends Component {
+  render() {
+    return (
+      <h1>Checklist</h1>
+    )
+  }
+}
+
+export default Checklist;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Checklist from './components/Checklist';
+import css from './index.sass';
+
+ReactDOM.render(<Checklist />, document.getElementById('root'));
