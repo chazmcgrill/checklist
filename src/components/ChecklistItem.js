6fe8248deb5a5edafebf4325cef9985d71326bf8
chazmcgrill <charlietaylordev@gmail.com>
2018-01-02 13:21:05 +0000

    Add Checkbox / Delete Hover Display
    Added hover display on delete button and checkbox div with styling

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const ChecklistItem = ({content, onToggle, onDelete}) => (
   <li>
-    <span
-      style={{textDecoration: content.completed ? 'line-through' : 'none'}}
-      onClick={onToggle}
-    >{content.content}</span>
-    <span onClick={onDelete}>X</span>
+    <span onClick={onToggle}>
+      <div style={{background: content.completed ? 'black' : 'none'}}></div>
+      <span style={{textDecoration: content.completed ? 'line-through' : 'none'}}>
+        {content.content}
+      </span>
+    </span>
+    <span className="delete-button" onClick={onDelete}>&times;</span>
   </li>
 );
 
